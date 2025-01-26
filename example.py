079ec94a22a04f21082ef683bf3f9653c32c9d25
Szabó Zsófia <szabo.zsofia.school@gmail.com>
2025-01-27 00:28:19 +0100

    example file

---
+++ example.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+
+from solve_methods import *
+from differential_equations import *
+from pysindy_methods import *
+
+init = [1.0]
+time = [0,5]
+step_size = 0.00001
+method = 'euler'
+
+#Differenciálegyenlet
+e = ExampleDifferentialEquations()
+diff_eq = e.logistic_growth
+
+so = SolveODE(diff_eq,time,init,step_size)
+mtx = so.get_matrix_with_noise(method)
+t = so.create_time_points()
+
+pm = PysindyFunctions(mtx,t,threshold=0.2)
+pm.print_model_equations()
