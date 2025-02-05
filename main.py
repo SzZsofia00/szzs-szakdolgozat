8431b2da6125ccad3b6a9372b837c6bdfa1fe4b4
Szabó Zsófia <szabo.zsofia.school@gmail.com>
2025-02-05 10:25:20 +0100

    debug

--- main.py
+++ main.py
@@ -12,19 +12,7 @@ parameters = {
     "threshold": 0.02
 }
 
-# apl = Application(parameters)
-# model = apl.sindy_model()
-# # apl.create_table_of_solutions(model)
-# print(apl.squared_deviation(model))
-
-symb_init = [sp.Symbol('x')]
-nm = NumericalMethods(parameters["diff_eq"], 0, symb_init, parameters["step_size"])
-result = getattr(nm, parameters["methodNM"])()[0]
-print(type(result))
-print(result)
-print([type(elem) for elem in result])
-
-for i, expr in enumerate(result):
-    print(f"Element {i}: Type={type(expr)}, Value={expr}")
-    if isinstance(expr, np.ndarray):
-        print(f"  └ Element {i} contains another NumPy array!")
+apl = Application(parameters)
+model = apl.sindy_model()
+# apl.create_table_of_solutions(model)
+print(apl.squared_deviation(model))
